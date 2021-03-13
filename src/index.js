3504fd80a5bc86c44b4d927011091e765c9317f8
Nilender-Andrey <nilenderav@mail.ru>
2021-03-13 17:04:17 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split(""); // str теперь массив
+  arr = [];
+
+for ( let i = 0; i < bracketsConfig.length; i++ ) {
+  for ( let j = 0; j < bracketsConfig[i].length; j++ ) {
+    arr.push( bracketsConfig[i] [j] );
+    }
+}
+console.log(arr);
+
+bracketsConfig = arr.join('');
+
+/* убрал пустые ячейки */
+
+for(let i=str.length; i>=0; i--){
+  if (str[i] === " ") str.splice(i, 1);
+  }
+
+for (let j=0; j<bracketsConfig.length; j+=2){
+  
+  for (let i = 0; i<str.length; i++){
+    
+    if (str[i] == bracketsConfig[j] && str[i+1] == bracketsConfig[j+1]){
+      
+      
+      str.splice(i, 2); 
+      i=-2; j=0; 
+    }
+
+  }
+}
+return str == '';
 }
